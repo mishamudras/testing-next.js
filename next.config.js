4ece191b149e38cd88fa22bd35dfb4e643c030f8
mishamudras <ladymishamirabela@email.cz>
2024-11-01 00:52:13 +0100

    Update Next.js config with PostCSS and Tailwind support

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // přidání podpory pro Tailwind CSS
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['postcss-loader']
-    });
-    return config;
-  }
-};
-
-module.exports = nextConfig;
+// next.config.js
+const config = {
+  // konfigurace
+}
+module.exports = config
